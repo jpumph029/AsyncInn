9fe2fb5357b7d02bc2686ecb460154e67e815799
Joseph <pumphrey.joseph@gmail.com>
2019-01-27 13:41:34 -0800

    added enum and navigationa properties in Room class

--- AsyncInn/AsyncInn/Models/Room.cs
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -10,5 +10,15 @@ namespace AsyncInn.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public int Layout { get; set; }
+
+        public ICollection<HotelRoom> HotelRoom { get; set; }
+
+        public ICollection<RoomAmenities> RoomAmenities { get; set; }
+    }
+    public enum Layout
+    {
+        Studio,
+        OneBedroom,
+        TwoBedroom
     }
 }

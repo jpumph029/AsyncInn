ffc03942a8f389e72c8e18758eff2fcfca0b664b
Joseph <pumphrey.joseph@gmail.com>
2019-01-27 14:11:50 -0800

    set up configuration

--- AsyncInn/AsyncInn/Models/HotelRoom.cs
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -8,7 +8,7 @@ namespace AsyncInn.Models
     public class HotelRoom
     {
         public int HotelID{ get; set; }
-        public int HotelNumber { get; set; }
+        public int RoomNumber { get; set; }
         public decimal RoomID { get; set; }
         public decimal Rate { get; set; }
         public decimal PetFriendly { get; set; }
